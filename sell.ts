9b721ff042b326622c664566e32f4a35092e3e9f
15077693d <oscaryiu.lapsang@gmail.com>
2023-09-20 00:28:26 +0800

    feat: add todo sell.ts

---
+++ sell.ts
@@ -0,0 +1,3 @@
+// @todo: add sell logic
+// faucet: https://faucet.quicknode.com/solana/devnet
+// sell: https://www.quicknode.com/guides/solana-development/nfts/how-to-create-a-solana-nft-collection-using-candy-machine-v3-and-typescript
